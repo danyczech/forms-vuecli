056144aef3a9df93985f8db731ccfcab13ae5dcf
danakal <dana.kalcikova@centrum.cz>
2023-02-17 15:30:56 +0100

    validation and v-model on component

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,8 +1,9 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{invalid:userNameValidity === 'invalid'}">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName"/>
+      <input id="user-name" name="user-name" type="text" v-model.trim="userName" @blur="validateInput"/>
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -47,6 +48,9 @@
       </div>
     </div>
     <div class="form-control">
+      <RatingControl v-model="rating"></RatingControl>
+    </div>
+    <div class="form-control">
       <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm" />
       <label for="confirm-terms">Agree to terms of use?</label>
     </div>
@@ -57,7 +61,12 @@
 </template>
 
 <script>
+import RatingControl from "./RatingControl.vue"
+
 export default {
+  components: {
+    RatingControl
+  },
   data(){
     return{
       userName:'',
@@ -65,7 +74,9 @@ export default {
       referrer: 'google',
       interest: [],
       how: null,
-      confirm: false
+      confirm: false,
+      userNameValidity: 'pending',
+      rating: null,
     }
   },
   methods: {
@@ -76,6 +87,14 @@ export default {
       this.interest=[];
       this.how=null;
       this.confirm=false;
+      this.rating=null;
+    },
+    validateInput(){
+      if (this.userName === ''){
+        this.userNameValidity = 'invalid';
+      } else {
+        this.userNameValidity = 'valid';
+      }
     }
   }
 
@@ -94,6 +113,13 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+.form-control.invalid label {
+  color: red;
 }
 
 label {
